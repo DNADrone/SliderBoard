54c8932cc82081209c05174cd6c5380588065f85
Dipayan Paul <game.designer.dipayan@gmail.com>
2016-11-20 21:58:24 +0530

    Initial commit

---
+++ SliderBoard/Source/SliderBoard/SliderBoard.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SliderBoard.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SliderBoard, "SliderBoard" );
---
+++ SliderBoard/Source/SliderBoard/SliderBoardGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SliderBoardGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLIDERBOARD_API ASliderBoardGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
